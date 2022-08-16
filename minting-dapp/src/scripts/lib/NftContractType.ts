dc01781a94cbf7a39b55d6346fed0e024ad78cd6
iamseancorcoran <iamseancorcoran@gmail.com>
2022-08-16 19:41:28 +0100

    darragh changes

--- minting-dapp/src/scripts/lib/NftContractType.ts
+++ minting-dapp/src/scripts/lib/NftContractType.ts
@@ -1,7 +1,7 @@
-// The name below ("AllowTest") should match the name of your Solidity contract.
+// The name below ("TeamPiss") should match the name of your Solidity contract.
 // It can be updated using the following command:
 // yarn rename-contract NEW_CONTRACT_NAME
 // Please DO NOT change it manually!
-import { AllowTest as NftContractType } from '../../../../smart-contract/typechain/index';
+import { TeamPiss as NftContractType } from '../../../../smart-contract/typechain/index';
 
 export default NftContractType;
